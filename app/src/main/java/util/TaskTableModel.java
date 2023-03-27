c56dc266a29713f89a4b123a15e238a14a39dd11
mylenaverspeelt <mylena49@hotmail.com>
2023-03-27 08:26:34 -0300

    aula 16G: torna os campos da coluna 3 da tabela editavel

--- app/src/main/java/util/TaskTableModel.java
+++ app/src/main/java/util/TaskTableModel.java
@@ -7,11 +7,8 @@ import javax.swing.table.AbstractTableModel;
 import model.Task;
 
 //CLASSE PRA DEFINIR A TABELA DE TAREFAS QUE VAO SER EXIBIDAS
-
 //ESSA CLASSE É FILHA DESSA DEFAULT, DAI JÁ VEM OS 3 PRIMEIROS METODOS OBRIGATORIOS, AI  SÓ SOBRESCREVE
-
 //ESSA CLASSE É FILHA DESSA DEFAULT, DAI JÁ VEM OS 3 PRIMEIROS METODOS OBRIGATORIOS.
-
 public class TaskTableModel extends AbstractTableModel {
 
     String[] columns = {"Name", "Description", "Deadline", "Completed", "Edit", "Delete"};
@@ -29,14 +26,18 @@ public class TaskTableModel extends AbstractTableModel {
         return columns.length;
     }
 
-
 //metodo pra aparecer os nomes das colunas. é um dos metodos herdados da classe pai
-
 //metodo pra aparecer os nomes das colunas
-
     @Override
     public String getColumnName(int columnIndex) {
-    return columns[columnIndex];
+        return columns[columnIndex];
+    }
+
+    @Override
+
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+
+        return columnIndex == 3;
     }
 
 //metodo pra pegar as informações de cada item da linha
@@ -50,7 +51,6 @@ public class TaskTableModel extends AbstractTableModel {
             case 2:
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
                 return dateFormat.format(tasks.get(rowIndex).getDeadline());
-                return tasks.get(rowIndex).getDeadline();
             case 3:
                 return tasks.get(rowIndex).isIsCompleted();
             case 4:
